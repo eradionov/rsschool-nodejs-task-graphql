9197a43706c87c534b44886328e5af5c814164ac
eugene.radionov <eugene.radionov@jagaad.it>
2023-07-24 18:22:39 +0200

    implement: depth limit

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -3,8 +3,12 @@ import {createGqlResponseSchema, gqlResponseSchema} from './schemas.js';
 import {graphql} from "graphql/graphql.js";
 import {PrismaClient} from "@prisma/client";
 import {schema} from './resolvers.js';
+import {specifiedRules, validate} from "graphql/validation/index.js";
+import {parse, Source} from "graphql/language/index.js";
+import depthLimit from "graphql-depth-limit";
 
 const prisma = new PrismaClient();
+const GRAPHQL_QUERY_DEPTH_LIMIT = 5;
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -16,9 +20,25 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         200: gqlResponseSchema,
       },
     },
-    async handler(req) {
+    async handler(req,reply) {
       const { query, variables } = req.body;
       const context = { prisma };
+
+      const documentAST = parse(new Source(query, 'Request parser'));
+
+      const validationErrors = validate(
+          schema,
+          documentAST,
+          [
+            ...specifiedRules,
+            depthLimit(GRAPHQL_QUERY_DEPTH_LIMIT)
+        ]);
+
+      if (validationErrors.length > 0) {
+        await reply.status(500).send({data: null, errors: validationErrors});
+
+        return;
+      }
 
       return await graphql({
         schema: schema,
